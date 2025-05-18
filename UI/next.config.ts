0d6be6a3b9c81de0af8400b794d1b3a566149a20
shubhamGrey <sk101294@gmail.com>
2025-05-18 07:46:58 +0530

    Add deployment workflow for Hostinger via FTP; update next.config.ts and package.json for static site export

--- UI/next.config.ts
+++ UI/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  output: "export",
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Image Optimization
+  },
 };
 
 export default nextConfig;
