2042711be144c0b84ba4c9a4a0f08dc2019dc193
shubhamkumar74 <shubhamkumar74@deloitte.com>
2025-05-25 22:07:46 +0530

    Update content section to encourage users to start adding content

--- UI/src/app/page.tsx
+++ UI/src/app/page.tsx
@@ -6,7 +6,8 @@ const Home: React.FC = () => {
     <div>
       <Container component="main" sx={{ py: 4, minHeight: "60vh" }}>
         <Typography variant="body1" align="center">
-          This is the content section. Add your content here.
+          This is the content section. Add your content here. Start adding
+          content
         </Typography>
       </Container>
     </div>
