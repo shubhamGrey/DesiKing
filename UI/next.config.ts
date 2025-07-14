03245bed797806cd218441207e151a3483bb3a1f
shubhamkumar74 <shubhamkumar74@deloitte.com>
2025-07-14 09:32:48 +0530

    feat: Add remote patterns for image optimization in Next.js configuration

--- UI/next.config.ts
+++ UI/next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cloud.agronexis.com"],
+    remotePatterns: [
+      {
+        protocol: "http", // Or 'https' depending on the actual protocol used by cloud.agronexis.com
+        hostname: "cloud.agronexis.com",
+        port: "",
+        pathname: "/s/**", // Adjust the pathname pattern if needed to match your file structure
+      },
+    ],
   },
 };
 
