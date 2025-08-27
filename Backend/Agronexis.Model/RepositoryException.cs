a7570906251a87c143cd869ea624fa003df1fa0b
hakumar-madihum <harsh.kumar@madihum.com>
2025-08-28 01:45:54 +0530

    fix: product api changes and exception handle

---
+++ Backend/Agronexis.Model/RepositoryException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+public class RepositoryException : Exception
+{
+    public string CorrelationId { get; }
+    public HttpStatusCode StatusCode { get; }
+
+    public RepositoryException(
+        string message,
+        string correlationId,
+        Exception innerException = null)
+        : base(message, innerException)
+    {
+        CorrelationId = correlationId;
+    }
+}
