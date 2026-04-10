8711f76f09f6278d0d9d27287d6df5cd8c749245
shkumar-madihum <shubham.kumar@madihum.com>
2026-04-10 22:47:56 +0530

    fixed image issue

--- UI/next.config.ts
+++ UI/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
+        protocol: "https",
         hostname: "cloud.desikingspices.com",
       },
     ],
